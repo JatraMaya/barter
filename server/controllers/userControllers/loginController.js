7bfa7dc188b3db80c6a8e5dd8f50c96a6a00df8d
JatraMaya <ahmad.f.iksan@gmail.com>
2021-01-04 12:33:37 +0800

    finalizing login controller

--- server/controllers/userControllers/loginController.js
+++ server/controllers/userControllers/loginController.js
@@ -1,19 +1,28 @@
 const { Op } = require("sequelize");
 const { User } = require("../../models");
-// const { comparePassword } = require("../../helpers/bcryptHelper");
-// const { sign } = require("jsonwebtoken");
+const { comparePassword } = require("../../helpers/bcryptHelper");
+const { tokenize } = require("../../helpers/jwtHelper");
 const checkCredential = require("../../helpers/credentialHelper");
 
 const Login = async (req, res) => {
+    const { password } = req.body;
     const { username, email } = checkCredential(req.body);
+    // Check user existance on database
     const isUserExist = await User.findOne({
         where: {
             [Op.or]: [{ username }, { email }],
         },
     });
-    const userData = JSON.parse(JSON.stringify(isUserExist));
-    console.log(userData);
-    return res.status(200).json(isUserExist);
+    // Throw error message if user not found
+    if (!isUserExist) return res.status(500).json({ error: "User unauthorize!" });
+    const user = JSON.parse(JSON.stringify(isUserExist));
+    const isPasswordValid = await comparePassword(password, user.password);
+    if (isPasswordValid) {
+        const token = tokenize(user);
+        user.token = token;
+        return res.status(200).json(user);
+    }
+    return res.status(500).json({ error: "User unauthorize!" });
 };
 
 module.exports = Login;
