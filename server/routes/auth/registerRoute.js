2eda9c46030d5d12836044507c2fb91024a5ad1e
JatraMaya <ahmad.f.iksan@gmail.com>
2021-01-04 11:34:31 +0800

    Do some cleaning on register route

--- server/routes/auth/registerRoute.js
+++ server/routes/auth/registerRoute.js
@@ -1,22 +1,6 @@
 const router = require("express").Router();
-// const {User} = require("../../models");
-// const {hashPassword} = require("../../helpers/bcryptHelper")
-const registerUser = require("../../controllers/userControllers/registerController")
+const { Register } = require("../../controllers/userControllers");
 
-
-router.post("/user", registerUser)
-
-// router.post("/user", async (req, res) => {
-//     const { fullname, username, email, password } = req.body;
-//     const passwordHash = await hashPassword(password)
-//     const user = await User.create({
-//         fullname,
-//         username,
-//         email,
-//         password:passwordHash,
-//     });
-
-//     return res.send(user);
-// });
+router.post("/user", Register);
 
 module.exports = router;
