9a1073aa056fba8d4230c45273969000b9755e26
JatraMaya <ahmad.f.iksan@gmail.com>
2021-01-05 21:21:48 +0800

    Adding Navbar component to main Vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="w-screen h-screen bg-red-600">
+        <Navbar />
         <h1>Text</h1>
     </div>
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+    components: {
+        Navbar,
+    },
+};
+</script>
