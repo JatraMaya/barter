22ca052f9ec5a739425a25c022d84b5cc9f6ddc6
JatraMaya <ahmad.f.iksan@gmail.com>
2021-01-05 21:15:50 +0800

    extending tailwind rotate utilities

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
     purge: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            rotate: {
+                "25": "25deg",
+                "-25": "-25deg",
+            },
+        },
     },
     variants: {
         extend: {},
